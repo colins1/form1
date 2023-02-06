b00f6ab386801c47edf56a6ae312b50756d4511c
colins1 <maxrozhkov1@gmail.com>
2023-02-06 09:35:38 +0300

    add

--- src/component/ColorCheck.js
+++ src/component/ColorCheck.js
@@ -6,22 +6,25 @@ class ColorCheck extends React.Component {
         code : ''
     }
 
-    hex2rgb(e) {
-        e.preventDefault();
-        let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(e);
-        return result ? {
-            r: parseInt(result[1], 16),
-            g: parseInt(result[2], 16),
-            b: parseInt(result[3], 16)
-        } : null;
+    hex2rgb = (e) => {
+        let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(e.target.value);
+        if (result) {
+           let results = {
+                r: parseInt(result[1], 16),
+                g: parseInt(result[2], 16),
+                b: parseInt(result[3], 16)
+            }
+            this.setState({backgraund: `rgb(${results.r},${results.g},${results.b})`, code: results});
+            return result;
+        }
     }
 
 
     render() {
         return (
-            <div>
+            <div style={{backgroundColor: this.state.backgraund}}>
                 <input onChange={this.hex2rgb} />
-                <p>{this.state.code}</p>
+                <p>{this.state.code.map( (itm) => `${itm}, `)}</p>
             </div>
         )
     }
