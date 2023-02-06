d78a9629e00b76a320923b1e906d2e428130fdaa
colins1 <maxrozhkov1@gmail.com>
2023-02-06 20:25:35 +0300

    add

--- src/component/ColorCheck.js
+++ src/component/ColorCheck.js
@@ -3,31 +3,46 @@ import React from 'react';
 class ColorCheck extends React.Component {
     state = {
         backgraund : 'white',
-        code : ''
+        vision : '',
+    }
+
+    componentWillMount = (cr) => {
+        document.body.style.backgroundColor = cr;
     }
 
     hex2rgb = (e) => {
-        let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(e.target.value);
-        if (result) {
-           let results = {
-                r: parseInt(result[1], 16),
-                g: parseInt(result[2], 16),
-                b: parseInt(result[3], 16)
+        if (e.target.selectionEnd === 7) {
+            let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(e.target.value);
+            if (result) {
+                this.componentWillMount(result.input);
+            let results = {
+                    r: parseInt(result[1], 16),
+                    g: parseInt(result[2], 16),
+                    b: parseInt(result[3], 16)
+                }
+                this.setState({backgraund: `rgb(${results.r},${results.g},${results.b})`, vision: 1});
+                return result;
             }
-            this.setState({backgraund: `rgb(${results.r},${results.g},${results.b})`, code: results});
-            return result;
+            else {
+                this.setState({backgraund: `red`, vision: 2});
+                this.componentWillMount(`red`);
+            }
+        }else
+        {
+            this.setState({backgraund: `white`, vision: ''});
+            this.componentWillMount(`white`);
         }
     }
 
 
     render() {
         return (
-            <div style={{backgroundColor: this.state.backgraund}}>
+            <div>
                 <input onChange={this.hex2rgb} />
-                <p>{this.state.code.map( (itm) => `${itm}, `)}</p>
+                <p>{this.state.vision === 2 ? 'Ошибка' : this.state.vision === '' ? '' : this.state.backgraund}</p>
             </div>
         )
     }
-}
+}     
 
 export default ColorCheck
